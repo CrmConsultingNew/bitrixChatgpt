de5c5f9bb9348a6f2f2750fb3d631d04d16c7d52
ErrNotNill <andreevivan8891@gmail.com>
2025-02-24 12:46:35 +0300

    fsome fixes

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"bitrix_app/backend/routes"
 	"bitrix_app/medi"
 	"bitrix_app/metrologiya"
+	"bitrix_app/webinar"
 	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/xuri/excelize/v2"
@@ -19,6 +20,8 @@ import (
 )
 
 func main() {
+
+	webinar.StartTgBot()
 
 	if err := godotenv.Load(filepath.Join(".env")); err != nil {
 		log.Print("No .env file found")
