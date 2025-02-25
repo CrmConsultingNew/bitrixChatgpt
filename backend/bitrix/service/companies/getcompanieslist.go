b851c1d357dacc46bf9830fe3b11875c6ac26d6f
ErrNotNill <andreevivan8891@gmail.com>
2025-02-25 13:29:58 +0300

    .gitignore

--- backend/bitrix/service/companies/getcompanieslist.go
+++ backend/bitrix/service/companies/getcompanieslist.go
@@ -34,7 +34,6 @@ func CompaniesHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonData)
-	log.Println("ended")
 }
 
 func GetCompanyByID(companyID, authKey string) (Company, error) {
